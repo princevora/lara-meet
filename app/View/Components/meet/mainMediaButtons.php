02cb4fd2726861663cbfc6354eede5cb4c577295
Prince <princevora684@gmail.com>
2024-11-18 16:08:49 +0530

    add params

--- app/View/Components/meet/mainMediaButtons.php
+++ app/View/Components/meet/mainMediaButtons.php
@@ -8,13 +8,46 @@ use Illuminate\View\Component;
 
 class mainMediaButtons extends Component
 {
+    /**
+     * @var ?string $class
+     */
     public ?string $class;
+
+    /**
+     * @var bool $addMicGroupButton 
+     */
+    public bool $addMicGroupButton = false;
+
+    /**
+     * @var mixed $micGroupButtonContent
+     */
+    public mixed $micGroupButtonContent = null;
+
+    /**
+     * @var bool $addCameraGroupButton 
+     */
+    public bool $addCameraGroupButton = false;
+
+    /**
+     * @var mixed $cameraGroupButtonContent
+     */
+    public mixed $cameraGroupButtonContent = null;
 
     /**
      * Create a new component instance.
      */
-    public function __construct(?string $class = "media-btns mb-3")
-    {
+    public function __construct
+    (
+        ?string $class = "media-btns mb-3",
+        $addMicGroupButton = false,
+        $addCameraGroupButton = false,
+        $micGroupButtonContent = "",
+        $cameraGroupButtonContent = ""
+    ) {
+        $this->addMicGroupButton = $addMicGroupButton;
+        $this->addCameraGroupButton = $addCameraGroupButton;
+        $this->micGroupButtonContent = $micGroupButtonContent;
+        $this->cameraGroupButtonContent = $cameraGroupButtonContent;
         $this->class = $class;
     }
 
