dab066ed54adad64bcb9868eddd043a635aa19e7
Prince <princevora684@gmail.com>
2024-11-24 12:17:55 +0530

    add enumereate for speaker

--- resources/views/livewire/user/meet/room.blade.php
+++ resources/views/livewire/user/meet/room.blade.php
@@ -56,6 +56,13 @@
                     .bg-blue span {
                         color: #062e6f !important;
                     }
+
+                    .text-ellipsis {
+                        white-space: nowrap;
+                        overflow: hidden;
+                        text-overflow: ellipsis;
+                    }
+
                 </style>
 
                 @slot('micSlot')
@@ -67,20 +74,19 @@
                     </button>
                     <div class="ignore-class">
                         <div data-popover id="popover-click-microphone" role="tooltip"
-                            class="absolute z-10 invisible inline-block w-64 text-sm text-gray-400 transition-opacity duration-300 rounded-lg shadow-sm opacity-0 border-gray-600 bg-gray-800">
-                            <div
-                                class="px-3 py-2 bg-gray-100 border-b rounded-t-lg dark:border-gray-600 bg-transparent">
-                                <h3 class="font-semibold text-gray-900 dark:text-white">Change Microphone Device</h3>
+                            class="absolute z-10 invisible inline-block w-auto text-sm text-gray-400 transition-opacity duration-300 rounded-lg shadow-sm opacity-0 border-gray-600 bg-gray-800">
+                            <div class="px-3 py-2 bg-gray-100 border-b rounded-t-lg dark:border-gray-600 bg-transparent">
+                                <h3 class="font-semibold text-gray-900 dark:text-white">Change Microphone And Speaker Device</h3>
                             </div>
                             <div class="px-3 py-2">
                                 <!-- Microphone Dropdown-->
-                                <div class="relative inline-block text-left w-full" data-device-related='microphone'>
+                                <div class="relative flex gap-2 text-left w-full" data-device-related='microphone'>
                                     <button disabled data-tooltip-placement="top" id="microphoneDropdownButton"
                                         data-dropdown-toggle="microphoneDropdown"
                                         class="!h-auto dropdown-device flex px-4 py-2 text-sm font-medium text-gray-400 bg-gray-900 border border-dark rounded-full shadow-md hover:bg-gray-950 focus:outline-none ring-2 ring-indigo-500 focus:ring-indigo-900 !w-full sm:!w-full disabled:bg-gray-950 disabled:text-gray-950 disabled:border-gray-800 disabled:shadow-md disabled:opacity-70">
                                         <span class="mr-2 inline-flex gap-1 justify-center">
                                             <span class="material-icons-outlined text-[18px]">mic</span>
-                                            <span id="info-mic">
+                                            <span id="info-mic" class="whitespace-nowrap">
                                                 Microphone
                                             </span>
                                         </span>
@@ -97,6 +103,33 @@
                                         role="menu" aria-labelledby="microphoneDropdownButton">
                                         <div id="audioinput-option-container" class="" role="none">
                                             <!-- Dynamic options will be appended here -->
+                                        </div>
+                                    </div>
+
+                                    <!-- Speakers Dropdown -->
+                                    <div class="relative inline-block text-left w-full sm:w-auto"
+                                        data-device-related='microphone'>
+                                        <button disabled id="speakersDropdownButton" data-dropdown-toggle="speakersDropdown"
+                                            class="!h-auto dropdown-device flex px-4 py-2 text-sm font-medium text-gray-400 bg-gray-900 border border-dark rounded-full shadow-md hover:bg-gray-950 focus:outline-none ring-2 ring-indigo-500 focus:ring-indigo-900 !w-full sm:!w-full disabled:bg-gray-950 disabled:text-gray-950 disabled:border-gray-800 disabled:shadow-md disabled:opacity-70">
+                                            <span class="mr-2 inline-flex gap-1">
+                                                <span class="material-icons-outlined text-[18px]">volume_up</span>
+                                                <span id="info-speaker" class="whitespace-nowrap">
+                                                    Speakers / Headphones
+                                                </span>
+                                            </span>
+                                            <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                viewBox="0 0 24 24" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                    d="M19 9l-7 7-7-7" />
+                                            </svg>
+                                        </button>
+
+                                        <!-- Speakers Options (positioned to open on top) -->
+                                        <div id="speakersDropdown"
+                                            class="hidden z-10 w-72 mb-2 bg-dark border font-medium border-dark rounded-md shadow-xl top-full"
+                                            role="menu" aria-labelledby="speakersDropdownButton">
+                                            <div id="audiooutput-option-container" class="py-1" role="none">
+                                            </div>
                                         </div>
                                     </div>
 
@@ -149,8 +182,8 @@
                 </svg>
             </button>
             <button class="btn btn-dark btn-circle" data-bs-toggle="tooltip" title="Adjust volume">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18" class="bi bi-volume-up"
-                    style="width: 16px; height: 16px;">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18"
+                    class="bi bi-volume-up" style="width: 16px; height: 16px;">
                     <path
                         d="M10.836.357a1.978 1.978 0 0 0-2.138.3L3.63 5H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h1.63l5.07 4.344a1.985 1.985 0 0 0 2.142.299A1.98 1.98 0 0 0 12 15.826V2.174A1.98 1.98 0 0 0 10.836.357Zm2.728 4.695a1.001 1.001 0 0 0-.29 1.385 4.887 4.887 0 0 1 0 5.126 1 1 0 0 0 1.674 1.095A6.645 6.645 0 0 0 16 9a6.65 6.65 0 0 0-1.052-3.658 1 1 0 0 0-1.384-.29Zm4.441-2.904a1 1 0 0 0-.289 1.384 9.418 9.418 0 0 1 0 11.936 1 1 0 1 0 1.673 1.096A11.387 11.387 0 0 0 20 9a11.387 11.387 0 0 0-2.284-6.548 1 1 0 0 0-1.384-.29Z" />
                 </svg>
