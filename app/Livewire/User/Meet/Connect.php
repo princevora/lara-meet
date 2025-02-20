c5b6ac7fe283f3888a3f3f586707502b313db97b
princevora <princevora684@gmail.com>
2025-02-20 19:05:10 +0530

    update add name ui

--- app/Livewire/User/Meet/Connect.php
+++ app/Livewire/User/Meet/Connect.php
@@ -8,16 +8,33 @@ use Livewire\Component;
 
 class Connect extends Component
 {
+    /**
+     * @var Meeting
+     */
     private Meeting $meeting;
 
-    public function mount(Request $request, Meeting $meeting)
+    /**
+     * @var string
+     */
+    public string $code;
+
+    /**
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Meeting $meeting
+     * @return void
+     */
+    public function mount(Request $request, Meeting $meeting, string $code)
     {
         $this->meeting = $meeting;
+        $this->code = $code;
         $this->meeting
             ->where('code', $request->code)
             ->firstOrFail();
     }
-    
+
+    /**
+     * @return \Illuminate\Contracts\View\View
+     */
     public function render()
     {
         return view('livewire.user.meet.connect');
--- resources/views/livewire/user/meet/connect.blade.php
+++ resources/views/livewire/user/meet/connect.blade.php
@@ -3,5 +3,11 @@
         <!-- Left: Meeting Card (70% of the grid) -->
         <x-meet.video-container />
 
+        <!-- Right: Connect Button -->
+        <div class="flex items-center justify-center">
+            <a href="{{ route('meet.room', ['code' => $code]) }}" class="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg shadow">
+                Connect to Meeting
+            </a>
+        </div>
     </div>
 </div>
