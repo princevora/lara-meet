3d2153f6d9f2a50e2060f43c55ba49ef478d5be7
princevora <princevora684@gmail.com>
2025-04-03 12:40:31 +0530

    update home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -95,7 +95,7 @@
                 </a>
                 <p class="mb-3 font-normal text-gray-400">Login to access the Lara Meet App</p>
                 <a href="#" class="inline-flex font-medium items-center text-blue-600 hover:underline">
-                    See our guideline
+                    Login Here..
                     <svg class="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                         fill="none" viewBox="0 0 18 18">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
