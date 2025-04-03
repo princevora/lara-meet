ff6ca4ad081b4239973e234dd5e73f19aec0c49e
princevora <princevora684@gmail.com>
2025-04-03 12:31:42 +0530

    update users migration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -49,38 +49,62 @@
             </p>
         </header>
 
-        <!-- Main Content -->
-        <main>
-            <div class="bg-gradient-to-br from-gray-800 via-gray-900 to-gray-800 rounded-2xl shadow-2xl p-8">
-                <!-- Join Meeting Section -->
-                <div class="mb-8">
-                    <h2
-                        class="text-3xl text-transparent font-bold bg-clip-text bg-gradient-to-r from-gray-300 to-purple-400 mb-6 text-center">
-                        Join a Meeting</h2>
-                    <div class="flex flex-col sm:flex-row gap-4 items-center">
-                        <input type="text" placeholder="Enter meeting code"
-                            class="w-full px-5 py-3 rounded-lg bg-gray-700 text-white text-lg focus:ring-4 focus:ring-purple-400 outline-none transition">
-                        <button
-                            class="px-8 py-3 text-lg font-semibold  bg-blue-800 text-white rounded-lg shadow-lg hover:scale-105 transform transition focus:ring-4 focus:ring-blue-500 focus:outline-none">
-                            Join
-                        </button>
+        @auth
+            <!-- Main Content -->
+            <main>
+                <div class="bg-gradient-to-br from-gray-800 via-gray-900 to-gray-800 rounded-2xl shadow-2xl p-8">
+                    <!-- Join Meeting Section -->
+                    <div class="mb-8">
+                        <h2
+                            class="text-3xl text-transparent font-bold bg-clip-text bg-gradient-to-r from-gray-300 to-purple-400 mb-6 text-center">
+                            Join a Meeting</h2>
+                        <div class="flex flex-col sm:flex-row gap-4 items-center">
+                            <input type="text" placeholder="Enter meeting code"
+                                class="w-full px-5 py-3 rounded-lg bg-gray-700 text-white text-lg focus:ring-4 focus:ring-purple-400 outline-none transition">
+                            <button
+                                class="px-8 py-3 text-lg font-semibold  bg-blue-800 text-white rounded-lg shadow-lg hover:scale-105 transform transition focus:ring-4 focus:ring-blue-500 focus:outline-none">
+                                Join
+                            </button>
 
+                        </div>
                     </div>
+
+                    <!-- Divider -->
+                    <div class="relative my-10">
+                        <div class="absolute inset-0 flex items-center">
+                            <div class="w-full border-t border-gray-700"></div>
+                        </div>
+                        <div class="relative flex justify-center">
+                            <span class="bg-gray-800 px-4 text-gray-400">OR</span>
+                        </div>
+                    </div>
+
+                    <livewire:user.home.start-meeting lazy />
                 </div>
-
-                <!-- Divider -->
-                <div class="relative my-10">
-                    <div class="absolute inset-0 flex items-center">
-                        <div class="w-full border-t border-gray-700"></div>
-                    </div>
-                    <div class="relative flex justify-center">
-                        <span class="bg-gray-800 px-4 text-gray-400">OR</span>
-                    </div>
-                </div>
-
-                <livewire:user.home.start-meeting lazy />
+            </main>
+        @else
+            <div class="max-w-sm p-6 mx-auto border rounded-lg shadow-sm border-gray-700">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="bi bi-person-video2" viewBox="0 0 16 16">
+                    <path d="M10 9.05a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5" />
+                    <path
+                        d="M2 1a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2zM1 3a1 1 0 0 1 1-1h2v2H1zm4 10V2h9a1 1 0 0 1 1 1v9c0 .285-.12.543-.31.725C14.15 11.494 12.822 10 10 10c-3.037 0-4.345 1.73-4.798 3zm-4-2h3v2H2a1 1 0 0 1-1-1zm3-1H1V8h3zm0-3H1V5h3z" />
+                </svg>
+                <a href="#">
+                    <h5 class="mb-2 text-2xl font-semibold tracking-tight text-white">Not Logged In Yet?</h5>
+                </a>
+                <p class="mb-3 font-normal text-gray-400">Login to access the Lara Meet App</p>
+                <a href="#" class="inline-flex font-medium items-center text-blue-600 hover:underline">
+                    See our guideline
+                    <svg class="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                        fill="none" viewBox="0 0 18 18">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778" />
+                    </svg>
+                </a>
             </div>
-        </main>
+
+        @endauth
 
         <!-- Footer -->
         <footer class="mt-12 text-center text-gray-500 text-sm">
