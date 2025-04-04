400160bada15726ca2b4093ef0191f4125b8cd43
princevora <princevora684@gmail.com>
2025-04-04 18:30:26 +0530

    update login and register page

--- resources/views/livewire/user/auth/register.blade.php
+++ resources/views/livewire/user/auth/register.blade.php
@@ -1,4 +1,4 @@
-<div class="min-h-screen flex items-center justify-center">
+<div class="min-h-screen flex items-center justify-center bg-gray-900">
     <form wire:submit="register" class="w-full max-w-md mx-auto p-6 bg-gray-800 rounded-lg shadow-lg">
         <h1 class="text-xl text-gray-200 text-center m-2 py-2">Register Here</h1>
         <div class="mb-5">
@@ -38,12 +38,18 @@
         <div class="flex items-center my-5">
             <input wire:model="remember_me" checked id="checked-checkbox" type="checkbox" value=""
                 class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-            <label for="checked-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember Me</label>
+            <label for="checked-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember
+                Me</label>
         </div>
 
         <button type="submit"
             class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
             Register new account
         </button>
+        <div class="my-5">
+            <a href="{{ route('user.auth.login') }}" class="text-blue-400 text-center underline">
+                Already Have An Account?
+            </a>
+        </div>
     </form>
 </div>
