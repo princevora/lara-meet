9ae767c753073468a26314b059a2b6e15c9b6ae7
princevora <princevora684@gmail.com>
2025-04-09 12:14:35 +0530

    update the room chats modal for adding a primary uuid

--- database/migrations/2025_04_09_064031_create_room_chats_table.php
+++ database/migrations/2025_04_09_064031_create_room_chats_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::create('room_chats', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->foreignUuid('room_id')
                 ->constrained('rooms')
                 ->onDelete('cascade')
